a1bfc0568b88f963418ac2e2738cf834d040c191
Josh Boyle <joshua.boyle@accenture.com>
2016-11-30 18:50:41 -0800

    Removed some more stuff and created a skeleton Use class for our lab

---
+++ core/src/main/java/kpfedtraining/core/use/TextImageUse.java
@@ -0,0 +1,13 @@
+package kpfedtraining.core.use;
+  
+import com.adobe.cq.sightly.WCMUsePojo;
+  
+public class TextImageUse extends WCMUsePojo {
+    private String lowerCaseTitle;
+    private String lowerCaseDescription;
+  
+    @Override
+    public void activate() throws Exception {
+
+    }
+}
